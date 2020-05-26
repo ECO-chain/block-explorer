3bd5c0e3c9562c9726260edb07939383104665a4
Napong T <58070033@kmitl.ac.th>
2020-05-26 14:13:16 +0700

    Update api model

--- src/api/types.ts
+++ src/api/types.ts
@@ -22,3 +22,30 @@ export interface Status {
   network: String,
   reward: Number
 }
+
+export interface Block {
+  height: Number,
+  size: Number,
+  hash: String,
+  time: Number,
+  txlength: Number,
+  poolInfo: Object,
+  isMainChain: Boolean,
+  minedBy: String
+}
+
+export interface Blocks {
+  blocks: Block[],
+  length: Number,
+  pagination: Pagination
+}
+
+export interface Pagination {
+  next: String,
+  prev: String,
+  currenTs: Number,
+  current: String,
+  isToday: Boolean,
+  more: Boolean,
+  moreTs: Number
+}
