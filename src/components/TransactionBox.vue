576cfe8c3f0fcc1c1541fa8c4f0a788f1c7e4d30
Napong T <58070033@kmitl.ac.th>
2020-06-11 15:26:51 +0700

    Add TransactionBox component

---
+++ src/components/TransactionBox.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="block-global p-3 my-3 rounded-lg">
+    <b-row class="align-items-end mb-3">
+      <b-col cols="12" md class="text-truncate">
+        <b-icon
+          class="mr-2 toggle-more"
+          :icon="moreDetail ? 'dash-square' : 'plus-square'"
+          @click="toggleMore"
+        ></b-icon>
+        {{ tx.blockhash }}
+      </b-col>
+      <b-col cols="12" md class="text-md-right small">mined {{ tx.time | timeFormat('LLL') }}</b-col>
+    </b-row>
+    <hr />
+    <b-row>
+      <b-col cols="12">
+        <b-row class="justify-content-center no-gutters">
+          <b-col cols="12" lg>
+            <!-- Coin based -->
+            <div v-if="tx.isCoinBase">
+              <b-row
+                class="align-items-center no-gutters"
+                v-for="(vout, index) in tx.vout"
+                :key="index"
+              >
+                <b-col cols="8" class="text-truncate">
+                  <router-link to="/address">Unparsed address [{{ index }}]</router-link>
+                  <b-collapse v-model="moreDetail" class="tx-collapse">
+                    <p class="tx-detail">Type</p>
+                    <p class="tx-detail">scriptPubKey {{ vout.scriptPubKey.asm }}</p>
+                  </b-collapse>
+                </b-col>
+                <b-col cols="4" class="text-right small text-purple-light">{{ vout.value }} ECOC</b-col>
+              </b-row>
+            </div>
+
+            <!-- Award / Normal Transaction -->
+            <div v-else>
+              <b-row
+                class="align-items-center no-gutters"
+                v-for="(vin, index) in tx.vin"
+                :key="index"
+              >
+                <b-col cols="8" class="text-truncate">
+                  <router-link to="/address">{{ vin.addr }}</router-link>
+                  <b-collapse v-model="moreDetail" class="tx-collapse">
+                    <p class="tx-detail">Confirmations: {{ tx.confirmations }}</p>
+                  </b-collapse>
+                </b-col>
+                <b-col cols="4" class="text-right small text-purple-light">{{ vin.value }} ECOC</b-col>
+              </b-row>
+            </div>
+          </b-col>
+
+          <b-col cols="12" lg="1" class="text-center">
+            <b-icon-arrow-right-short class="h3 mb-0 rotate-md-90"></b-icon-arrow-right-short>
+          </b-col>
+          <b-col cols="12" lg>
+            <div v-if="tx.isCoinBase">No Inputs (Newly Generated Coins)</div>
+
+            <div v-else>
+              <b-row
+                class="align-items-center no-gutters"
+                v-for="(vout, index) in tx.vout"
+                :key="index"
+              >
+                <b-col cols="8" class="text-truncate">
+                  <router-link
+                    to="/address"
+                  >{{ 'addresses' in vout.scriptPubKey ? vout.scriptPubKey.addresses[0] : `Unparsed address ${index}` }}</router-link>
+                  <b-collapse v-model="moreDetail" class="tx-collapse">
+                    <p
+                      class="tx-detail"
+                    >Type {{ 'type' in vout.scriptPubKey ? vout.scriptPubKey.type : '' }}</p>
+                    <p class="tx-detail">scriptPubKey {{ vout.scriptPubKey.asm }}</p>
+                  </b-collapse>
+                </b-col>
+                <b-col cols="4" class="text-right small text-purple-light">{{ vout.value }} ECOC (U)</b-col>
+              </b-row>
+            </div>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12" md class="text-center text-md-right">
+        <div
+          class="d-inline-block small text-purple-light mx-3 mr-md-0"
+        >CONFIRMATIONS: {{ tx.confirmations }}</div>
+        <div class="d-inline-block small text-purple-light mx-3 mr-md-0">{{ tx.valueOut }} ECOC</div>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+// eslint-disable-next-line no-unused-vars
+import { Tx, TxValueIn } from '../api/transaction/type'
+
+@Component({})
+export default class TransactionBox extends Vue {
+  @Prop() tx!: Tx
+  moreDetail = false
+
+  mounted() {}
+  toggleMore() {
+    this.moreDetail = !this.moreDetail
+  }
+
+  // duplicatedValueInAddr(vin: TxValueIn[]) {
+  //   // eslint-disable-next-line no-unused-vars
+  //   let vinTotalVal = 0
+  //   // eslint-disable-next-line no-unused-vars
+  //   let newObj = {}
+  //   for (let i = 0; i < vin.length; i++) {
+  //     for (let j = i+1; j < vin.length; j++) {
+  //       if (vin[i].addr === vin[j].addr) {
+  //         vinTotalVal += vin[i].value! + vin[j].value!
+  //         newObj = Object.assign(vin[i], vin[j])
+  //         dupVin.push(vin[j])
+  //         vin.splice(j, i)
+  //       }
+  //     }
+  //   }
+
+  //   console.log('dupVin', dupVin)
+  //   console.log('vin', vin)
+  // }
+}
+</script>
+
+<style lang="scss" scoped>
+.toggle-more {
+  cursor: pointer;
+}
+
+.tx-detail {
+  font-size: smaller;
+  color: rgb(198, 198, 198);
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.tx-collapse {
+  padding-top: 0.75rem;
+  padding-left: 1rem;
+}
+</style>
--- src/pages/Block.vue
+++ src/pages/Block.vue
@@ -5,8 +5,7 @@
         <b-col cols="12">
           <div class="group-head my-3 text-center text-md-left">
             <h2 class="head-page my-0">
-              <span>B</span>
-              lock #{{ block.height }}
+              <span>B</span>lock #{{ block.height }}
             </h2>
             <p class="my-0 text-truncate">
               BLOCKHASH:
@@ -149,7 +148,7 @@
                   </b-col>
                   <b-col cols="6">
                     <div class="my-1 text-right text-truncate">
-                      <router-link :to="`/block${block.previousblockhash}`">{{ block.height - 1 }}</router-link>
+                      <a :href="`/block/${block.previousblockhash}`">{{ block.height - 1 }}</a>
                     </div>
                   </b-col>
                 </b-row>
@@ -161,7 +160,7 @@
                   </b-col>
                   <b-col cols="6">
                     <div class="my-1 text-right text-truncate">
-                      <router-link :to="`/block${block.nextblockhash}`">{{ block.height + 1 }}</router-link>
+                      <a :href="`/block/${block.nextblockhash}`">{{ block.height + 1 }}</a>
                     </div>
                   </b-col>
                 </b-row>
@@ -175,169 +174,8 @@
             <h3 class="head-global my-3">Transactions</h3>
           </div>
 
-          <div class="block-global p-3 my-3 rounded-lg">
-            <b-row class="align-items-end mb-3">
-              <b-col
-                cols="12"
-                md
-                class="text-truncate"
-              >ac8a3348a82e4aedf6177264e216a81a032fc9df2fd6feb32e56495037b9ee4c</b-col>
-              <b-col cols="12" md class="text-md-right small">mined April 6, 2020 8:19:52 PM</b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12">
-                <b-row class="justify-content-center no-gutters">
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">309.49 ECOC</b-col>
-                    </b-row>
-                  </b-col>
-                  <b-col cols="12" lg="1" class="text-center">
-                    <b-icon-arrow-right-short class="h3 mb-0 rotate-md-90"></b-icon-arrow-right-short>
-                  </b-col>
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">Unparsed address [0]</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">0 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">319.49 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">ELYmtXKWZFCg6GTUg2WXW1GhEfPdbYSJw8</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">40 ECOC (U)</b-col>
-                    </b-row>
-                  </b-col>
-                </b-row>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12" md class="text-center text-md-right">
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">CONFIRMATIONS: 43</div>
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">359.49 ECOC</div>
-              </b-col>
-            </b-row>
-          </div>
-          <!-- END .block-global -->
+          <TransactionBox v-for="(tx, index) in txs.txs" :key="index" :tx="tx"></TransactionBox>
 
-          <div class="block-global p-3 my-3 rounded-lg">
-            <b-row class="align-items-end mb-3">
-              <b-col
-                cols="12"
-                md
-                class="text-truncate"
-              >ac8a3348a82e4aedf6177264e216a81a032fc9df2fd6feb32e56495037b9ee4c</b-col>
-              <b-col cols="12" md class="text-md-right small">mined April 6, 2020 8:19:52 PM</b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12">
-                <b-row class="justify-content-center no-gutters">
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">309.49 ECOC</b-col>
-                    </b-row>
-                  </b-col>
-                  <b-col cols="12" lg="1" class="text-center">
-                    <b-icon-arrow-right-short class="h3 mb-0 rotate-md-90"></b-icon-arrow-right-short>
-                  </b-col>
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">Unparsed address [0]</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">0 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">319.49 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">ELYmtXKWZFCg6GTUg2WXW1GhEfPdbYSJw8</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">40 ECOC (U)</b-col>
-                    </b-row>
-                  </b-col>
-                </b-row>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12" md class="text-center text-md-right">
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">CONFIRMATIONS: 43</div>
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">359.49 ECOC</div>
-              </b-col>
-            </b-row>
-          </div>
-          <!-- END .block-global -->
-          <div class="block-global p-3 my-3 rounded-lg">
-            <b-row class="align-items-end mb-3">
-              <b-col
-                cols="12"
-                md
-                class="text-truncate"
-              >ac8a3348a82e4aedf6177264e216a81a032fc9df2fd6feb32e56495037b9ee4c</b-col>
-              <b-col cols="12" md class="text-md-right small">mined April 6, 2020 8:19:52 PM</b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12">
-                <b-row class="justify-content-center no-gutters">
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">309.49 ECOC</b-col>
-                    </b-row>
-                  </b-col>
-                  <b-col cols="12" lg="1" class="text-center">
-                    <b-icon-arrow-right-short class="h3 mb-0 rotate-md-90"></b-icon-arrow-right-short>
-                  </b-col>
-                  <b-col cols="12" lg>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">Unparsed address [0]</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">0 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">EKfT5eCujEDuMok5kYo8VkG95jRmAnmaaL</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">319.49 ECOC (U)</b-col>
-                    </b-row>
-                    <b-row class="align-items-center no-gutters">
-                      <b-col cols="8" class="text-truncate">
-                        <router-link to="/address">ELYmtXKWZFCg6GTUg2WXW1GhEfPdbYSJw8</router-link>
-                      </b-col>
-                      <b-col cols="4" class="text-right small text-purple-light">40 ECOC (U)</b-col>
-                    </b-row>
-                  </b-col>
-                </b-row>
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="12" md class="text-center text-md-right">
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">CONFIRMATIONS: 43</div>
-                <div class="d-inline-block small text-purple-light mx-3 mr-md-0">359.49 ECOC</div>
-              </b-col>
-            </b-row>
-          </div>
-          <!-- END .block-global -->
         </b-col>
       </b-row>
     </b-container>
@@ -347,19 +185,31 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import blocksModule from '@/api/blocks/index'
+import txModule from '@/api/transaction/index'
+import TransactionBox from '@/components/TransactionBox.vue'
 // eslint-disable-next-line no-unused-vars
 import { BlockDetail } from '../api/blocks/type'
+// eslint-disable-next-line no-unused-vars
+import { Txs } from '../api/transaction/type'
 
-@Component
+@Component({
+  components: {
+    TransactionBox,
+  }
+})
 export default class Block extends Vue {
   @Prop() hash!: string
 
   block: BlockDetail = {} as BlockDetail
+  txs: Txs = {} as Txs
 
   async mounted() {
     this.block = await blocksModule.getBlockDetail(this.hash)
 
+    this.txs = await txModule.getBlockTransactions(this.block.hash)
+
     console.log('block', this.block)
+    console.log('txs', this.txs)
   }
 }
 </script>
