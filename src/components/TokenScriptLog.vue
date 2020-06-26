2ff779df0d3ed0b01fcd6ed638ed6facd6dfb2c6
Napong T <58070033@kmitl.ac.th>
2020-06-26 14:47:22 +0700

    Add eng locale json file

--- src/components/TokenScriptLog.vue
+++ src/components/TokenScriptLog.vue
@@ -3,15 +3,13 @@
     <b-col cols="12" md="6">
       <div class="block-scroll p-3 my-3 rounded-lg">
         <h4 class="text-style-1 text-uppercase mb-2">{{ isToken ? 'Bytecode' : 'Contract Code' }}</h4>
-        <div class="content-scroll text-break small" v-if="byteCode">{{ byteCode }}</div>
-        <div class="content-scroll text-break small" v-else>Transaction</div>
+        <div class="content-scroll text-break small">{{ byteCode }}</div>
       </div>
     </b-col>
     <b-col cols="12" md="6">
       <div class="block-scroll p-3 my-3 rounded-lg">
         <h4 class="text-style-1 text-uppercase mb-2">Asm</h4>
-        <div class="content-scroll text-break small" v-if="byteCode">{{ asmCode }}</div>
-        <div class="content-scroll text-break small" v-else>Transaction....</div>
+        <div class="content-scroll text-break small">{{ asmCode }}</div>
       </div>
     </b-col>
   </b-row>
