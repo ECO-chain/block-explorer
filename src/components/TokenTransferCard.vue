9e693de7606f2789d33d39fb8b5e1c6b426d525b
Napong T <58070033@kmitl.ac.th>
2020-07-06 21:56:38 +0700

    Add infinite scroll to Token transfer & holders card

--- src/components/TokenTransferCard.vue
+++ src/components/TokenTransferCard.vue
@@ -1,45 +1,81 @@
 <template>
-  <b-card class="mb-3">
-    <b-row>
-      <b-col>
-        <b-card-text class="text-truncate transfer-hash font-weight-bolder">
-          <router-link
-            :to="{ name: 'transaction', params: { hash: transfer.tx_hash } }"
-          >{{ transfer.tx_hash }}</router-link>
-        </b-card-text>
-      </b-col>
-      <b-col class="text-right">{{ transfer.tx_time * 1000 | timeFromNow }}</b-col>
-    </b-row>
-    <b-row class="mt-2">
-      <b-col class="transfer-to">
-        <div class="text-truncate">
-          <router-link
-            :to="{ name: 'address', params: { addr: transfer.from } }"
-          >{{ transfer.from }}</router-link>
-        </div>
-        <i class="fas fa-arrow-right tx-arrow"></i>
-        <div class="text-truncate">
-          <router-link :to="{ name: 'address', params: { addr: transfer.to } }">{{ transfer.to }}</router-link>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <span class="transfer-label pr-2">Quantitiy</span>
-        {{ Number(transfer.value) | numberWithCommas(8) }}
-      </b-col>
-    </b-row>
-  </b-card>
+  <div>
+    <b-card class="mb-3" v-for="(transfer,index) in transfer.items" :key="index">
+      <b-row>
+        <b-col>
+          <b-card-text class="text-truncate transfer-hash font-weight-bolder">
+            <router-link
+              :to="{ name: 'transaction', params: { hash: transfer.tx_hash } }"
+            >{{ transfer.tx_hash }}</router-link>
+          </b-card-text>
+        </b-col>
+        <b-col class="text-right">{{ transfer.tx_time * 1000 | timeFromNow }}</b-col>
+      </b-row>
+      <b-row class="mt-2">
+        <b-col class="transfer-to">
+          <div class="text-truncate">
+            <router-link
+              :to="{ name: 'address', params: { addr: transfer.from } }"
+            >{{ transfer.from }}</router-link>
+          </div>
+          <i class="fas fa-arrow-right tx-arrow"></i>
+          <div class="text-truncate">
+            <router-link :to="{ name: 'address', params: { addr: transfer.to } }">{{ transfer.to }}</router-link>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <span class="transfer-label pr-2">Quantitiy</span>
+          {{ Number(transfer.value) | numberWithCommas(8) }}
+        </b-col>
+      </b-row>
+    </b-card>
+    <template v-if="transfer.items.length > 0">
+      <infinite-loading
+        slot="append"
+        @infinite="inifiniteTransfer"
+        force-use-infinite-wrapper="block-global"
+      ></infinite-loading>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import ecrc20Module from '@/api/ecrc20/index'
+import InfiniteLoading from 'vue-infinite-loading'
 // eslint-disable-next-line no-unused-vars
-import { TokenTransfer } from '../api/ecrc20/type'
+import { TokenTransfers } from '../api/ecrc20/type'
 
-@Component
+@Component({
+  components: {
+    InfiniteLoading
+  }
+})
 export default class TokenTransferCard extends Vue {
-  @Prop() transfer!: TokenTransfer
+  @Prop() transfer!: TokenTransfers
+  @Prop() addr!: string
+
+  async inifiniteTransfer($state: any) {
+    if (this.transfer.offset < this.transfer.count) {
+      setTimeout(async () => {
+        console.log('infinite transfers')
+        let newTransfers = await ecrc20Module.getTokenTransfers({
+          contractAddr: this.addr,
+          limit: 20,
+          offset: this.transfer.offset + 20
+        })
+        newTransfers.items.forEach(transfer => {
+          this.transfer.items.push(transfer)
+        })
+        this.transfer.offset = newTransfers.offset
+        $state.loaded()
+      }, 500)
+    } else {
+      $state.complete()
+    }
+  }
 }
 </script>
 
