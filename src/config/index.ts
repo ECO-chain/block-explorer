f85194227ff80fd851c205812fe640cdf6a202c7
Napong T <58070033@kmitl.ac.th>
2020-07-08 11:37:17 +0700

    Update config once again

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,5 +8,4 @@ const envName = process.env.NODE_ENV
 
 export const env =
   envName === 'production'
-    ? local
-    : [production, local].find(env => env.name === envName)
+    ? production : local
