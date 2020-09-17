0c19a2ad0b5637184bd865e014a8f9a018dce7ad
Bilal <bilal.mte@gmail.com>
2020-09-17 12:12:19 +0700

    get network from env variable

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,10 +2,11 @@ import { AppConfig } from '@/types'
 
 const prodURL = process.env.BASE_URL ? process.env.BASE_URL : '/'
 const prodSocket = process.env.SOCKET_URL ? process.env.SOCKET_URL : '/'
+const prodNetwork = process.env.NETWORK ? process.env.NETWORK : 'livenet'
 
 export default {
   name: 'production',
   baseURL: prodURL,
-  network: 'livenet',
+  network: prodNetwork,
   socketURL: prodSocket
 } as AppConfig
